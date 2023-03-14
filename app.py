fa7e875579336ac43b2eb48b89b7c4509dce007b
Kevin Ybarra <kevin.ybarra@gmail.com>
2023-03-13 21:42:31 -0500

    commit

--- app.py
+++ app.py
@@ -14,54 +14,70 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/project_data"
+        f"/api/v1.0/mlb_data"
     )
 
 
 def get_db_conn():
-    conn = sqlite3.connect('incomeCrime.db')
+    conn = sqlite3.connect('mlbSeason.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
-@app.route('/project_data')
+@app.route('/mlb_data')
 @cross_origin()
 def project_data():
     conn = get_db_conn()
-    posts = conn.execute('SELECT * FROM income_crime').fetchall()
+    posts = conn.execute('SELECT * FROM mlb_season_stats').fetchall()
     conn.close()
-    # posts = list(np.ravel(posts))
     data = []
     
     for post in posts:
-        crime_income_data = {}
-        crime_income_data["FIPS"] = post[0]
-        crime_income_data["county"] = post[1]
-        crime_income_data["state"] = post[2]
-        crime_income_data["crime_rate_per_100000"] = post[3]
-        crime_income_data["crime_rate_rank"] = post[4]
-        crime_income_data["murder"] = post[5]
-        crime_income_data["rape"] = post[6]
-        crime_income_data["robbery"] = post[7]
-        crime_income_data["aggravated_assault"] = post[8]
-        crime_income_data["burglary"] = post[9]
-        crime_income_data["larceny"] = post[10]
-        crime_income_data["motor_theft"] = post[11]
-        crime_income_data["arson"] = post[12]
-        crime_income_data["population_x"] = post[13]
-        crime_income_data["total_violent_crime"] = post[14]
-        crime_income_data["household_income_rank"] = post[15]
-        crime_income_data["per_capita_income"] = post[17]
-        crime_income_data["household_income"] = post[18]
-        crime_income_data["family_income"] = post[19]
-        crime_income_data["num_of_households"] = post[20]
-        crime_income_data["lat"] = post[21]
-        crime_income_data["lng"] = post[22]
-        crime_income_data["FIPS_ST"] = post[23]
-        crime_income_data["FIPS_CTY"] = post[24]
+        mlb_season_data = {}
+        mlb_season_data["year"] = post[0]
+        mlb_season_data["league_id"] = post[1]
+        mlb_season_data["division_id"] = post[2]
+        mlb_season_data["rank"] = post[3]
+        mlb_season_data["games_played"] = post[4]
+        mlb_season_data["home_games"] = post[5]
+        mlb_season_data["wins"] = post[6]
+        mlb_season_data["losses"] = post[7]
+        mlb_season_data["division_winner"] = post[8]
+        mlb_season_data["wild_card_winner"] = post[9]
+        mlb_season_data["world_series_winner"] = post[10]
+        mlb_season_data["runs_scored"] = post[11]
+        mlb_season_data["at_bats"] = post[12]
+        mlb_season_data["hits"] = post[13]
+        mlb_season_data["doubles"] = post[14]
+        mlb_season_data["triples"] = post[15]
+        mlb_season_data["homeruns"] = post[17]
+        mlb_season_data["walks"] = post[18]
+        mlb_season_data["strikeouts_by_batters"] = post[19]
+        mlb_season_data["stolen_bases"] = post[20]
+        mlb_season_data["caught_stealing"] = post[21]
+        mlb_season_data["batters_hit_by_pitch"] = post[22]
+        mlb_season_data["sacrifice_flies"] = post[23]
+        mlb_season_data["opponents_runs_scored"] = post[24]
+        mlb_season_data["earned_runs_allowed"] = post[25]
+        mlb_season_data["earned_run_average"] = post[26]
+        mlb_season_data["complete_games"] = post[27]
+        mlb_season_data["shutouts"] = post[28]
+        mlb_season_data["saves"] = post[29]
+        mlb_season_data["outs_pitches"] = post[30]
+        mlb_season_data["hits_allowed"] = post[31]
+        mlb_season_data["homeruns_allowed"] = post[32]
+        mlb_season_data["walks_allowed"] = post[33]
+        mlb_season_data["strikeouts_by_pitchers"] = post[34]
+        mlb_season_data["errors"] = post[35]
+        mlb_season_data["double_plays"] = post[36]
+        mlb_season_data["fielding_percentage"] = post[37]
+        mlb_season_data["team_name"] = post[38]
+        mlb_season_data["ball_park"] = post[39]
+        mlb_season_data["strikeouts_by_pitchers"] = post[40]
+        mlb_season_data["home_attendance"] = post[41]
 
 
-        data.append(crime_income_data)
+        data.append(mlb_season_data)
 
     return jsonify(data)
 
